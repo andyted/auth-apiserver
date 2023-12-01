8b7b390abc4ba6c85bdd199f23207517be306ca9
Andy Tay <soonwan@gmail.com>
2023-12-01 21:16:47 +0800

    First commit

---
+++ src/main/java/lab/fruit/authapiserver/AuthApiserverApplication.java
@@ -0,0 +1,13 @@
+package lab.fruit.authapiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthApiserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthApiserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lab/fruit/authapiserver/AuthApiserverApplicationTests.java
@@ -0,0 +1,13 @@
+package lab.fruit.authapiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApiserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
